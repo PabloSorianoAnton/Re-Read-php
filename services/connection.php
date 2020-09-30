aa7f294d2ce9f57f43256dcc62c462cacea051d6
Pablo Soriano Antón <10006712.joan23@fje.edu>
2020-09-30 17:50:34 +0200

    Ultima subida

--- services/connection.php
+++ services/connection.php
@@ -1,20 +1,33 @@
-<?
-// estilo por procedimientos
+<?php
+// ESTILO POR PROCEDIMIENTOS
 
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "reread-php";
+$db = "reread";
 
-// crear la conexion
-$conn = mysqli_connect($host,$user,$pass,$db)
+// Crear la conexión
+$conn = mysqli_connect($host, $user, $pass, $db);
 
-// checkear la conexion
+// Checkear la conexión
 if (!$conn) {
-    echo "Error: No se pudo conectar a MYSQL." . PHP_EOL;
+    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "Error de depuración: " . mysqli_connect_errno() . PHP_EOL;
     exit;
 } else {
-    mysqli_set_charset($conn, "utf8");
+	mysqli_set_charset($conn, "utf8");
 }
+
+/* //ESTILO ORIENTADO A OBJETOS
+// Crear la conexión
+$conn = new mysqli('localhost', 'root', '', 'imagenes');
+
+// Checkear la conexión
+if ($conn->connect_errno) {
+	die("Error: No se pudo conectar a MySQL: " . $conn->connect_errno);
+} else {
+	$conn->set_charset("utf8");
+}
+*/
+
 ?>
--- view/eBooks.php
+++ view/eBooks.php
@@ -1,17 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>CSS Website Layout</title>
+<title>Re-Read ebooks</title>
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--Estilos enlazados-->
-<link rel="stylesheet" type="text/css" href="../css/estilo.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="../css/styles.css">
+<script src="../js/code.js"></script>
 </head>
-<body>
+<body onload="descriptionImg()">
 
-<div class="logo">
-  Re-Read  
-</div>
+<div class="logo"><h1>Re-Read</h1></div>
 
 <div class="header">
   <h1>Re-Read</h1>
@@ -19,66 +17,70 @@
 </div>
 
 <div class="row">
-  
-  <div class="column left">
+  <div class="column middle">
     <div class="topnav">
       <a href="../index.php">Re-Read</a>
       <a href="libros.php">Libros</a>
-      <a href="eBooks.php">eBooks</a>
+      <a href="ebooks.php" class="active">eBooks</a>
     </div>
-    <h3>Toda la actualidad en eBook</h3>
-    <!---eBooks con descripcion-->
-     <div class="ebook">
-      <a href="https://www.casadellibro.com/libro-cell/9788483465219/1162268"><img src="../img/ebook1.jpeg" alt="eBook 1">
-      <div>A través de los teléfonos móviles se envía un mensaje que convierte a todos en esclavos asesinos...</div>
-      </a>
+    <div class="textpage">
+      <h3>Toda la actualidad en eBook</h3>
+      <!--
+      <div class="gallery">
+        <img src="../img/cell.jpeg" alt="Cell">
+        <div class="desc">A través de los teléfonos móviles se envía un mensaje que convierte a todos en esclavos asesinos...</div>
+      </div>
+      -->
+      <?php
+      // 1. Conexión con la base de datos	
+      include '../services/connection.php';
+
+      // 2. Selección y muestra de datos de la base de datos
+      $result = mysqli_query($conn, "SELECT Books.Description, Books.img, Books.Title FROM Books WHERE eBook != '0'");
+
+      if (!empty($result) && mysqli_num_rows($result) > 0) {
+      // datos de salida de cada fila	(fila = row)
+        $i=0;
+        while ($row = mysqli_fetch_array($result)) {
+          $i++;
+          echo "<div class='gallery'>";
+          // Añadimos la imagen a la página con la etiqueta img de HTML
+          echo "<img src=../img/".$row['img']." alt='".$row['Title']."'>";
+          // ---- Evolutivo:
+          echo "<div class='desc'>".$row['Description']." </div>";
+          // ---- Fin del evolutivo
+          echo "</div>";
+          if ($i%3=='0') {
+            echo "<div style='clear:both;'></div>";
+          }
+        }
+      } else {
+        echo "0 resultados";
+      }
+      ?>
     </div>
-    <!-- <div class="ebook">
-      <a href="https://www.casadellibro.com/libro-el-ciclo-del-hombre-lobo/9788499081281/1819674"><img src="../img/ebook2.jpeg" alt="eBook 2">
-      <div>Una escalofriante revisión del mito del hombre lobo por el rey de la literatura de terror...</div>
-      </a>
-    </div>
-    <div class="ebook">
-      <a href="https://www.casadellibro.com/libro-el-resplandor/9788490328729/2197218"><img src="../img/ebook3.jpeg" alt="eBook 3">
-      <div>Esa es la palabra que Danny había visto en el espejo. Y, aunque no sabía leer, entendió que era un mensaje de horror...</div>
-      </a>
-    </div>
-    <div class="ebook">
-      <a href="https://www.casadellibro.com/libro-doctor-sueno/9788401354809/2196951"><img src="../img/ebook4.jpeg" alt="eBook 3">
-      <div>Una novela que entusiasmará a los millones de lectores de El resplandor y que encantará...</div>
-      </a>
-    </div> -->
   </div>
+  <div class="column side">
+    <h2>Top ventas</h2>
+    <?php
+      // 1. Conexión con la base de datos	
+      //include '../services/connection.php';
 
-  <?php
-  // conexiona la base de datos
-  include '../services/connection.php';
+      // 2. Selección y muestra de datos de la base de datos
+      $result = mysqli_query($conn, "SELECT Books.Title FROM Books WHERE eBook != '0'");
 
-  // seleccion y muestra de datos de la base de datos
-  $result = mysqli_query($conn, "SELECT Books.Description, Books.img, Books.Title FROM Books WHERE eBook !='0'");
-
-  if (!empty($result) && mysqli_num_rows($result) > 0) {
-    // datos de salida de cada fila (fila = row)
-    while ($row = mysqli_fetch_array($result)) {
-      echo "<div class='ebook'>";
-      // añadimos la imagen a la pagina con la etiqueta img de html
-      echo "<img src=../img/".$row['img']." alt='".$row['Title']."'>";
-      // añadimos el titulo a la pagina con la etiqueta h2 de html 
-      // echo "<div class='desc'".$row['Title']."</div>;
-      echo "</div>";
-    }
-  }
-
-  ?>
-  
-  <div class="column right">
-    <h2>Side</h2>
-    <p>Cien años de soledad.</p>
-    <p>Crónica de una muerte anunciada.</p>
-    <p>El otoño del patriarca.</p>
-    <p>El general en su laberinto.</p>
+      if (!empty($result) && mysqli_num_rows($result) > 0) {
+      // datos de salida de cada fila	(fila = row)
+        while ($row = mysqli_fetch_array($result)) {
+          echo "<p>".$row['Title']."</p>";
+        }
+      } else {
+        echo "0 resultados";
+      }
+      ?>
   </div>
 </div>
-  
+
+
 </body>
 </html>
